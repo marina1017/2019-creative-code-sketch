4a8ad133167617e742663d26c022cbc5cf602aed
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-11-03 15:30:57 +0900

    試しにドーナッツを回転させる

--- src/01-typography/index.js
+++ src/01-typography/index.js
@@ -24,48 +24,47 @@
         var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.x = 0;
         camera.position.y = 0;
-        camera.position.z = 50;
+        camera.position.z = 1000;
         camera.lookAt(scene.position);
 
-        // スポットライト光源を作成
-        //new THREE.SpotLight(色, 光の強さ, 距離, 照射角, ボケ具合, 減衰率)
-        // const light = new THREE.SpotLight(0x00FF00, 4, 30, Math.PI / 6, 0, 0.5);
-        // light.position.x = 0;
-        // light.position.y = 0;
-        // light.position.z = 0;
-        // scene.add(light);
+        // 平行光源
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(1, 1, 1);
+        scene.add(directionalLight);
+        // ポイント光源
+        const pointLight = new THREE.PointLight(0xffffff, 2, 1000);
+        scene.add(pointLight);
+        const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+        scene.add(pointLightHelper);
 
-        // // ヘルパーを作成
-        // const lightHelper = new THREE.SpotLightHelper(light);
-        // scene.add(lightHelper);
-
-        //底を作る
-        ideyo_ita()
+        // ドーナツを作成
+        const geometry = new THREE.TorusGeometry(30, 10, 64, 10);
+        // マテリアルを作成
+        const material = new THREE.MeshToonMaterial({ color: 0x6699ff });
+        // メッシュを作成
+        const mesh = new THREE.Mesh(geometry, material);
+        // 3D空間にメッシュを追加
+        scene.add(mesh);
 
         tick();
 
         // 毎フレーム時に実行されるループイベントです
         function tick() {
-            //geometry.rotation.y += 0.01;
-            renderer.render(scene, camera); // レンダリング
+            // メッシュを回転させる
+            mesh.rotation.x += 0.01;
+            mesh.rotation.y += 0.01;
+
+            // ライトを周回させる
+          pointLight.position.set(
+            500 * Math.sin(Date.now() / 500),
+            500 * Math.sin(Date.now() / 1000),
+            500 * Math.cos(Date.now() / 500)
+          );
+            // レンダリング
+            renderer.render(scene, camera); 
 
         requestAnimationFrame(tick);
       }
     }
-
-    //板を出す
-function ideyo_ita(){
-    //1.ジオメトリを新しく作る→PlaneGeometry - 板作るよ
-    var planeGeometry = new THREE.PlaneGeometry(10, 10, 20, 20);
-    //2.メッシュを作ります → 赤っぽいのにした
-    var planeMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFFFF, side: THREE.DoubleSide} );
-    //3.さっき作った1と2をTHREE.Mesh()の引数に入れてnew
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    //場所決めないと出てきてくれない
-    plane.position.x = 0;
-    plane.position.y = 0;
-    plane.position.z = 0;
-    scene.add(plane);
-}
   
   
