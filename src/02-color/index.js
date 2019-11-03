aae46d4575b97902850fc46c0b2f79c44784e0a7
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-11-03 16:41:45 +0900

    Readmeを入れる

--- src/02-color/index.js
+++ src/02-color/index.js
@@ -82,28 +82,6 @@
           scene.add(floor);
         }
 
-        // // マス目を作成
-        // {
-        //   // 立方体のマテリアルとジオメトリを作成
-        //   const material = new THREE.MeshStandardMaterial({
-        //     color: 0x22dd22,
-        //     roughness: 0.1,
-        //     metalness: 0.2
-        //   });
-        //   const geometry = new THREE.BoxGeometry(45, 45, 45);
-        //   // 立方体を複数作成しランダムに配置
-        //   for (let i = 0; i < 60; i++) {
-        //     const box = new THREE.Mesh(geometry, material);
-        //     box.position.x = Math.round((Math.random() - 0.5) * 19) * 50 + 25;
-        //     box.position.y = 25;
-        //     box.position.z = Math.round((Math.random() - 0.5) * 19) * 50 + 25;
-        //     // 影の設定
-        //     box.receiveShadow = true;
-        //     box.castShadow = true;
-        //     scene.add(box);
-        //   }
-        // }
-
         // ドーナツを作成
         const geometry = new THREE.BoxGeometry(40, 40, 40);
         // マテリアルを作成
