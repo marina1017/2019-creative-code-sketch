cba9cb08d684dfb3c2a24223d87b426cd73164b1
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-11-10 23:13:32 +0900

    ビルドの修正

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # 作ったもの
 
+## 第二回 色
+
+### コンセプト
+光の三原色と同様に、WebGLの世界でも三色が重なると本当に白になるのか調べてみた
+
+### 作品
+#### CodePen
+https://codepen.io/marina1017/pen/abbEPqj
+
+#### github-page
+https://marina1017.github.io/2019-creative-code-sketch/src/02-color/
+
+### コード
+https://github.com/marina1017/2019-creative-code-sketch/tree/master/src/02-color
+
+## 第三回 パーティクル
+### コンセプト
+dat.GUIをつかって制御できるパーティクルを目指した。
+マウスで空間をぐるぐる回せるようにした
+geometryの「geometry.verticesNeedUpdate」はレンダリング後に毎回falseになることに気が付かず、geometryの位置をうごかすことにやたら苦労した。
+https://stackoverflow.com/questions/24531109/three-js-vertices-does-not-update
+
+### 使用技術
+WebGL(Treee.js)
+dat.GUI
+
+## 作品
+#### github-page
+https://marina1017.github.io/2019-creative-code-sketch/src/03-particle/
+
+### コード
+https://github.com/marina1017/2019-creative-code-sketch/tree/master/src/03-particle
--- sketches/03-particle/03-particle.e31bb0bc.js
+++ sketches/03-particle/03-particle.e31bb0bc.js
@@ -121,43 +121,76 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // シーン
-  var scene = new THREE.Scene(); // レンダラー
+  // シーン---------------
+  var scene = new THREE.Scene(); //---------------------
+  // レンダラー---------------
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement); // カメラ
+  document.body.appendChild(renderer.domElement); //---------------------
+  // カメラ---------------
 
   var camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight);
   camera.position.set(0, 0, 100);
-  scene.add(camera); // マウス
+  scene.add(camera); //---------------------
+  //オブジェクト---------------
+  // 形状データを作成
 
-  var controls = new THREE.OrbitControls(camera); // ライト
+  var geometry = new THREE.Geometry(); // 配置する範囲
+
+  var SIZE = 3000; // 配置する個数
+
+  var LENGTH = 10000; // geometryにランダムに点をいれていく
+
+  for (var i = 0; i < LENGTH; i++) {
+    var particle = new THREE.Vector3(SIZE * (Math.random() - 0.5), SIZE * (Math.random() - 0.5), SIZE * (Math.random() - 0.5));
+    particle.velocity = new THREE.Vector3(0, Math.random(1, 10), Math.random(1, 10));
+    geometry.vertices.push(particle);
+  }
+
+  geometry.verticesNeedUpdate = true;
+  geometry.elementNeedUpdate = true; //---------------------
+  // マテリアル---------------
+
+  var material = new THREE.PointsMaterial({
+    // 一つ一つのサイズ
+    size: 10,
+    // 色
+    color: 0xffffff,
+    //ブレンド(加算)
+    blending: THREE.AdditiveBlending,
+    //透過するか
+    transparent: true,
+    clipIntersection: true
+  }); //---------------------
+  // particle systemをつくる---------------
+
+  var mesh = new THREE.Points( //第一引数は,geometry
+  geometry, //第一引数は,マテリアル
+  material);
+  scene.add(mesh); //---------------------
+  // マウス---------------
+
+  var controls = new THREE.OrbitControls(camera); //---------------------
+  // ライト---------------
 
   light = new THREE.DirectionalLight(0xcccccc, 1);
   light.position = new THREE.Vector3(0, 10, 10);
   ambient = new THREE.AmbientLight(0x333333);
   scene.add(light);
-  scene.add(ambient); // キューブ
-
-  var geometry = new THREE.BoxGeometry(12, 12, 12);
-  var c = '#c2dc94';
-  var material = new THREE.MeshPhongMaterial({
-    color: c
-  });
-  var cube = new THREE.Mesh(geometry, material);
-  cube.position.set(0, 0, 0);
-  scene.add(cube); // GUIパラメータ
+  scene.add(ambient); //---------------------
+  // GUIで変更できるようにする---------------
 
   var guiCtrl = function guiCtrl() {
     this.Camera_x = 0;
     this.Camera_y = 0;
     this.Camera_z = 100;
     this.Message = '';
-    this.color = "#c2dc94";
+    this.color = "#FFFFFF";
+    this.size = 10;
 
     this.alert = function () {
-      alert("サンプル");
+      alert("アラートのテスト");
     };
   };
 
@@ -168,6 +201,7 @@ function init() {
   folder.add(guiObj, 'Camera_y', 0, 100).onChange(setCameraPosition);
   folder.add(guiObj, 'Camera_z', 0, 100).onChange(setCameraPosition);
   folder.addColor(guiObj, 'color').onChange(setColor);
+  folder.add(guiObj, 'size', 10, 1000).onChange(setSize);
   folder.add(guiObj, 'alert');
   folder.open();
 
@@ -176,21 +210,40 @@ function init() {
   }
 
   function setColor() {
-    material = new THREE.MeshPhongMaterial({
-      color: guiObj.color
-    });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-  } // レンダリング
+    //マテリアル
+    var code = guiObj.color;
+    var red = parseInt(code.substring(1, 3), 16);
+    var green = parseInt(code.substring(3, 5), 16);
+    var blue = parseInt(code.substring(5, 7), 16);
+    mesh.material.color.r = red / 255;
+    mesh.material.color.g = green / 255;
+    mesh.material.color.b = blue / 255;
+  }
+
+  function setSize() {
+    mesh.material.size = guiObj.size;
+  } //---------------------
+  // レンダリング---------------------
 
 
   function render() {
-    requestAnimationFrame(render);
-    controls.update();
+    requestAnimationFrame(render); //particle system自体をまわす
+
+    mesh.rotation.y += 0.001; // meshに設定したgeometryの中に入っている点のy軸をランダムに動かすようにする
+
+    mesh.geometry.vertices.forEach(function (vertex) {
+      vertex.setY(vertex.y + vertex.velocity.y);
+    }); // geometryの「geometry.verticesNeedUpdate」はレンダリング後に毎回falseになるらしいのでtrueをここで設定している。
+    // https://stackoverflow.com/questions/24531109/three-js-vertices-does-not-update
+
+    mesh.geometry.verticesNeedUpdate = true; // マウスでカメラを操作するため
+
+    controls.update(); //シーンとカメラをいれる。
+
     renderer.render(scene, camera);
   }
 
-  render();
+  render(); //---------------------
 }
 },{}]},{},["index.js"], null)
 //# sourceMappingURL=/03-particle.e31bb0bc.js.map
