a63c3dba892152ee9c0288b4943da787c25a67c6
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-12-10 00:14:13 +0900

    テスト

--- src/03-particle/index.js
+++ src/03-particle/index.js
@@ -1,9 +1,7 @@
+console.log("/////////////")
 import * as THREE from 'three'
 import OrbitControls from 'three-orbitcontrols'
 import * as dat from 'dat.gui'
-
-import { MeshLine, MeshLineMaterial, } from 'three.meshline';
-import { Material } from 'three';
 
 window.addEventListener('DOMContentLoaded', init);
 
