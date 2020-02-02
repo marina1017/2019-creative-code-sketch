14836cc467b57a277f1f1e1d942bd8c690749f41
Marina Nakagawa <mari10172001@yahoo.co.jp>
2020-02-03 01:38:11 +0900

    Chrome Extentionのチュートリアル

---
+++ src/10-chromeExtension/background.js
@@ -0,0 +1,15 @@
+//これは、browserAction という機能を使って、 拡張アイコンがクリックされた時に、script.js スクリプトファイルを実行するようにしています。
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'developer.chrome.com'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
